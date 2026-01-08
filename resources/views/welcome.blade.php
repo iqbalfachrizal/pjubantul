db81887a73e7ca9f92b8b8aa022a6b26c83e8754
Bintang Jati <bintangid69@gmail.com>
2026-01-08 10:55:27 +0700

    add article landing

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,20 +59,76 @@
                         Penerangan Jalan Umum Kabupaten Bantul
                     </h2>
 
-                    <p class="mt-6 text-xl leading-8 text-white max-w-2xl mx-auto">
+                    <p class="mt-6 text-xl leading-8 text-white max-w-xl mx-auto">
                         Sistem pemetaan terpadu yang menampilkan informasi Lampu PJU, Tiang, dan KWH di seluruh wilayah
                         Kabupaten Bantul. Akurat, terstruktur, dan dapat diakses publik.
                     </p>
 
                     <div class="mt-10 flex justify-center gap-x-6">
                         <a href="#"
-                            class="rounded-md bg-blue-600 px-6 py-3 text-white font-semibold shadow-sm hover:bg-blue-500">
-                            Lihat Peta PJU
+                            class="rounded-4xl text-lg bg-white px-8 py-4 text-black font-semibold shadow-sm hover:bg-gray-200">
+                            Lihat Peta
+                        </a>
+                        <a href="#"
+                            class="rounded-4xl text-lg bg-blue-600 px-8 py-4 text-white font-semibold shadow-sm hover:bg-blue-500">
+                            Kirim Laporan
                         </a>
                     </div>
 
                 </div>
             </main>
+            <article>
+                <h3 class="mt-20 text-center md:text-4xl font-bold tracking-tight text-black">
+                    Platform Manajemen Aset PJU Kabupaten Bantul
+                </h3>
+
+                <p class="mt-6 text-center text-lg leading-8 font-base leading-relaxed text-gray-600 max-w-2xl mx-auto">
+                    Aplikasi WebGIS yang dirancang untuk membantu pemerintah dan masyarakat dalam memantau kondisi
+                    Penerangan Jalan Umum secara menyeluruh. Informasi disajikan secara visual, terpusat, dan mudah diakses.
+                </p>
+                <div class=" py-24 sm:py-32">
+                    <h3 class="mb-15 text-center md:text-4xl font-bold tracking-tight text-black">
+                        Statistik aset tahun 2026
+                    </h3>
+                    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+
+                        <dl class="grid grid-cols-2 gap-8 text-center lg:grid-cols-3">
+                            <div
+                                class="mmx-auto flex w-full max-w-md flex-col gap-y-6 bg-white p-12 rounded-3xl shadow-xl border border-gray-100 hover:shadow-2xl transition-all">
+                                <dt class="text-lg font-medium text-gray-600">Total Lampu</dt>
+                                <dd class="order-first text-4xl font-bold tracking-tight text-green-900 sm:text-6xl">
+                                    10.418
+                                </dd>
+                            </div>
+
+                            <div
+                                class="mx-auto flex w-full max-w-md flex-col gap-y-6 bg-white p-12 rounded-3xl shadow-xl border border-gray-100 hover:shadow-2xl transition-all">
+                                <dt class="text-lg font-medium text-gray-600">Total Tiang</dt>
+                                <dd class="order-first text-4xl font-bold tracking-tight text-red-900 sm:text-6xl">
+                                    10.131
+                                </dd>
+                            </div>
+
+                            <div
+                                class="mx-auto flex w-full max-w-md flex-col gap-y-6 bg-white p-12 rounded-3xl shadow-xl border border-gray-100 hover:shadow-2xl transition-all">
+                                <dt class="text-lg font-medium text-gray-600">Total Aset</dt>
+                                <dd class="order-first text-4xl font-bold tracking-tight text-purple-900 sm:text-6xl">
+                                    20.549
+                                </dd>
+                            </div>
+                        </dl>
+                    </div>
+                </div>
+
+                <h3 class="mb-6 text-center md:text-4xl font-bold tracking-tight text-black">
+                    Statistik aset tahun 2026
+                </h3>
+                <p class="text-center text-lg leading-8 font-base leading-relaxed text-gray-600 max-w-2xl mx-auto">
+                    Aplikasi WebGIS yang dirancang untuk membantu pemerintah dan masyarakat dalam memantau kondisi
+                    Penerangan Jalan Umum secara menyeluruh. Informasi disajikan secara visual, terpusat, dan mudah diakses.
+                </p>
+
+            </article>
         </div>
 
         </main>
